2761fc16a030f7de0967d47e3aa1acc39643fe9c
Peter Henell <peter.henell@klarna.com>
2015-03-26 09:55:54 +0100

    adding mvc playground

---
+++ mvc_learning/peterhenell.se/peterhenell.se/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace peterhenell.se.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+			
+			return View ();
+		}
+
+
+		public JsonResult SomeActionMethod() 
+		{ 
+			return Json(new {foo="bar", baz="Blech"}, JsonRequestBehavior.AllowGet);
+		}
+	
+	}
+}
+
